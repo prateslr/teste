87596a3601c034871a25532ca5e6048d0370c9e7
Lucas Prates <100208473+prateslr@users.noreply.github.com>
2024-10-10 17:05:48 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,9 @@ window.addEventListener('scroll', () => {
       }
   });
 });
+
+/* rolagem menu */
+window.addEventListener("scroll", function(){
+    let header = document.querySelector('#header')
+    header.classList.toggle('rolagem', window.scrollY > 100)
+})
